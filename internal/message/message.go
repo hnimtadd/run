75f1357a0c0c8aa24d0bee69c66946c0631f123c
Dat Nguyen <minhdat15012002@gmail.com>
2024-02-04 23:00:41 +0700

    WIP: working with request message

--- internal/message/message.go
+++ internal/message/message.go
@@ -1,10 +1,13 @@
 package message
 
+import "io"
+
 type MessageType byte
 
 const (
 	MessageTypeRequestRuntime MessageType = iota
 	MessageTypeRemoveRuntime
+	MessageTypeRequest
 )
 
 type Message struct {
@@ -15,4 +18,15 @@ type Message struct {
 type RequestRuntimeMessage struct {
 	DeploymentID string
 	Runtime      string
+}
+
+type RemoveRuntimeMessage struct {
+	DeploymentID string
+}
+
+type RequestMessage struct {
+	DeploymentID string
+	Body         io.Reader
+	Env          map[string]string
+	Args         []string
 }
