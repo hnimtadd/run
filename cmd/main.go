bdd284fdced76693a5b6bb8e79e08444f6b585c9
Dat Nguyen <minhdat15012002@gmail.com>
2024-03-06 14:18:10 +0700

    feat: update default main file

--- cmd/main.go
+++ cmd/main.go
@@ -2,11 +2,29 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
 
-	"github.com/hnimtadd/run/internal/manager"
+	"github.com/hnimtadd/run/internal/actrs"
+
+	"github.com/asynkron/protoactor-go/actor"
+	"github.com/joho/godotenv"
 )
 
 func main() {
-	server := manager.NewServer(nil)
-	fmt.Println(server)
+	if err := godotenv.Load(".env"); err != nil {
+		panic(err)
+	}
+
+	system := actor.NewActorSystem()
+	defer system.Shutdown()
+	s := actrs.NewServer(os.Getenv("WASM_ADDR"))
+	wasmPID := system.Root.Spawn(actor.PropsFromProducer(s))
+	fmt.Println(wasmPID)
+
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
+	signal.Notify(c, syscall.SIGTERM)
+	<-c
 }
