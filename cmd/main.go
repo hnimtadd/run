93914c2edd2396b09751cc20a5c72e697487dd54
Dat Nguyen <minhdat15012002@gmail.com>
2024-03-09 00:05:56 +0700

    chore: update main.go file that run locally

--- cmd/main.go
+++ cmd/main.go
@@ -7,6 +7,8 @@ import (
 	"syscall"
 
 	"github.com/hnimtadd/run/internal/actrs"
+	"github.com/hnimtadd/run/internal/api"
+	"github.com/hnimtadd/run/internal/store"
 
 	"github.com/asynkron/protoactor-go/actor"
 	"github.com/joho/godotenv"
@@ -19,10 +21,14 @@ func main() {
 
 	system := actor.NewActorSystem()
 	defer system.Shutdown()
-	s := actrs.NewServer(os.Getenv("WASM_ADDR"))
+	st := store.NewMemoryStore()
+	s := actrs.NewServer(fmt.Sprintf(":%v", os.Getenv("WASM_ADDR")), st)
 	wasmPID := system.Root.Spawn(actor.PropsFromProducer(s))
 	fmt.Println(wasmPID)
-
+	apiServer := api.NewServer(st)
+	go func() {
+		panic(apiServer.ListenAndServe(fmt.Sprintf(":%v", os.Getenv("API_ADDR"))))
+	}()
 	c := make(chan os.Signal, 1)
 	signal.Notify(c, os.Interrupt)
 	signal.Notify(c, syscall.SIGTERM)
