f0639a35b74cf441f31a882fa67c402baf98592e
Dat Nguyen <minhdat15012002@gmail.com>
2024-03-10 00:15:35 +0700

    feat: sdk now write status code, response len to stdOut of sandbox

---
+++ examples/go/build.sh
@@ -0,0 +1,2 @@
+GOOS=wasip1 GOARCH=wasm go build -o examples/go/example.wasm examples/go/example.go
+
--- sdk/sdk.go
+++ sdk/sdk.go
@@ -13,18 +13,6 @@ import (
 	"github.com/hnimtadd/run/pb/v1"
 )
 
-// var (
-// 	requestBuffer  []byte
-// 	responseBuffer []byte
-// )
-
-// type request struct {
-// 	Header http.Header
-// 	Method string
-// 	URL    string
-// 	Body   []byte
-// }
-
 type responseWriter struct {
 	buffer *bytes.Buffer
 	code   int
@@ -48,7 +36,8 @@ func (w *responseWriter) WriteHeader(status int) {
 	w.code = status
 }
 
-// unmarshal the mashaled request and process
+// unmarshal the marshaled request and process
+
 func Handle(h http.Handler) {
 	b, err := io.ReadAll(os.Stdin)
 	if err != nil {
@@ -60,6 +49,7 @@ func Handle(h http.Handler) {
 		log.Fatalf("sdk: cannot unmarshal the proto request, err: %v", err)
 	}
 
+	w := newResponseWriter()
 	r, err := http.NewRequest(req.GetMethod(), req.GetUrl(), bytes.NewReader(req.GetBody()))
 	if err != nil {
 		log.Fatalf("sdk: cannot create http request from given proto request, err: %v", err)
@@ -69,17 +59,12 @@ func Handle(h http.Handler) {
 		r.Header[header] = values.Fields
 	}
 
-	w := newResponseWriter()
-
 	h.ServeHTTP(w, r)
-	// write response to sandbox stdout
-	if _, err := io.Copy(os.Stdout, w.buffer); err != nil {
-		log.Panicf("sdk, cannot write response to sandbox stdout, err: %v", err)
-	}
-
 	// write response information to sandbox stdout, using for check valid response
+	_, _ = os.Stdout.Write(w.buffer.Bytes())
+
 	buf := make([]byte, 8)
 	binary.LittleEndian.PutUint32(buf[0:4], uint32(w.code))
 	binary.LittleEndian.PutUint32(buf[4:8], uint32(w.buffer.Len()))
-	os.Stdout.Write(buf)
+	_, _ = os.Stdout.Write(buf)
 }
