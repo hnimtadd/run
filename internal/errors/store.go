92f20847b6f2a954875b812878d6858775ed7594
Dat Nguyen <minhdat15012002@gmail.com>
2024-03-17 16:10:12 +0700

    chore: add store related errors

--- internal/errors/store.go
+++ internal/errors/store.go
@@ -7,4 +7,6 @@ var (
 	ErrEndpointNotExisted   = errors.New("given endpoint is not existed")
 	ErrDeploymentExisted    = errors.New("given deployment is existed")
 	ErrDeploymentNotExisted = errors.New("given deployment is not existed")
+	ErrDocumentNotFound     = errors.New("given document is not existed")
+	ErrDocumentDuplicated   = errors.New("given document is existed")
 )
