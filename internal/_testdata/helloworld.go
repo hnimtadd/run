0037f45b5a9e71286ef0ee72e6295f1a8fc86e5d
Dat Nguyen <minhdat15012002@gmail.com>
2024-03-17 16:10:41 +0700

    chore: update test file for test log parse

--- internal/_testdata/helloworld.go
+++ internal/_testdata/helloworld.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/hnimtadd/run/sdk"
@@ -9,8 +10,10 @@ import (
 func handle(w http.ResponseWriter, _ *http.Request) {
 	w.WriteHeader(http.StatusOK)
 	_, _ = w.Write([]byte("Hello world!"))
+	fmt.Println("hello, this is a request_log.go")
 }
 
+//export _start
 func main() {
 	sdk.Handle(http.HandlerFunc(handle))
 }
