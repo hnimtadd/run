942b88fdb9193965f102d1efede35164e7750821
Dat Nguyen <minhdat15012002@gmail.com>
2024-03-17 16:17:50 +0700

    chore: update local main file

--- cmd/main.go
+++ cmd/main.go
@@ -10,12 +10,14 @@ import (
 	"github.com/hnimtadd/run/internal/actrs"
 	"github.com/hnimtadd/run/internal/api"
 	"github.com/hnimtadd/run/internal/store"
+	"github.com/hnimtadd/run/internal/types"
 
 	"github.com/asynkron/protoactor-go/actor"
 	"github.com/asynkron/protoactor-go/cluster"
 	"github.com/asynkron/protoactor-go/cluster/clusterproviders/automanaged"
 	"github.com/asynkron/protoactor-go/cluster/identitylookup/disthash"
 	"github.com/asynkron/protoactor-go/remote"
+	"github.com/google/uuid"
 	"github.com/joho/godotenv"
 )
 
@@ -29,6 +31,7 @@ func init() {
 
 func main() {
 	st := store.NewMemoryStore()
+	MockStore(st)
 	mc := store.NewMemoryModCacher()
 
 	system := actor.NewActorSystem()
@@ -51,13 +54,13 @@ func main() {
 					Addr:  fmt.Sprintf(":%v", os.Getenv("WASM_ADDR")),
 					Store: st,
 				}),
-			actrs.NewRuntimeManagerKind(&actrs.RuntimeManagerConfig{
-				Store: st,
-				Cache: mc,
-			}), actrs.NewRuntimeKind(&actrs.RuntimeConfig{
-				Store: st,
-				Cache: mc,
-			}),
+			actrs.NewRuntimeManagerKind(),
+			actrs.NewRuntimeKind(
+				&actrs.RuntimeConfig{
+					Store:    st,
+					Cache:    mc,
+					LogStore: st,
+				}),
 		),
 	)
 
@@ -67,7 +70,7 @@ func main() {
 	pid := c.Get(os.Getenv("ACTOR_WASM_SERVER_ID"), actrs.KindServer)
 	fmt.Println(pid)
 
-	apiServer := api.NewServer(st)
+	apiServer := api.NewServer(st, st)
 	go func() {
 		panic(apiServer.ListenAndServe(fmt.Sprintf(":%v", os.Getenv("API_ADDR"))))
 	}()
@@ -75,4 +78,34 @@ func main() {
 	signal.Notify(exitCh, os.Interrupt)
 	signal.Notify(exitCh, syscall.SIGTERM)
 	<-exitCh
+}
+
+func MockStore(s *store.MemoryStore) {
+	endpoint := &types.Endpoint{
+		ID:      uuid.MustParse("c495fdf0-f8c9-41ad-befd-ff9af5d0d33d"),
+		Name:    "mockEndpoint",
+		Runtime: "go",
+		Environment: map[string]string{
+			"my_name": "my_name",
+		},
+	}
+	err := s.CreateEndpoint(endpoint)
+	if err != nil {
+		panic(err)
+	}
+
+	var blob []byte
+	blob, err = os.ReadFile("./examples/go/example.wasm")
+	if err != nil {
+		panic(err)
+	}
+	var deployment *types.Deployment
+	deployment, err = types.NewDeployment(endpoint, blob)
+	if err != nil {
+		panic(err)
+	}
+	err = s.CreateDeployment(deployment)
+	if err != nil {
+		panic(err)
+	}
 }
